37b21a9cfebc680e8e6c8684ca1dd006fa71ac84
HakeemAlavi <hakeem.alavi@strathmore.edu>
2023-09-17 17:51:41 +0300

    Added a sidebar for the user and admin home pages

--- admin-home.php
+++ admin-home.php
@@ -27,22 +27,26 @@ if($email != false && $password != false){
     <meta charset="UTF-8">
     <title><?php echo $fetch_info['name'] ?> | Home</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
     <style>
     @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
     nav{
-        padding-left: 100px!important;
-        padding-right: 100px!important;
+        padding-left: 50px!important;
+        padding-right: 50px!important;
+        padding-top: 15px!important;
+        padding-bottom: 15px!important;
         background: #3deb6c;
         font-family: 'Poppins', sans-serif;
     } 
     nav a.navbar-brand{
         color: #fff;
-        font-size: 30px!important;
+        font-size: 25px!important;
         font-weight: 500;
     }
     button a{
         color: #3deb6c;
         font-weight: 500;
+        border-radius: 16px;
     }
     button a:hover{
         text-decoration: none;
@@ -57,14 +61,190 @@ if($email != false && $password != false){
         font-size: 50px;
         font-weight: 600;
     }
+    * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: "Poppins", sans-serif;
+        }
+
+        
+
+        .sidebar {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 110px;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            background: rgba(255, 255, 255, 0.2);
+            backdrop-filter: blur(17px);
+            --webkit-backdrop-filter: blur(17px);
+            border-right: 1px solid rgba(255, 255, 255, 0.7);
+            transition: width 0.3s ease;
+        }
+
+        .sidebar:hover {
+            width: 260px;
+        }
+
+        .sidebar .logo {
+            color: #000;
+            display: flex;
+            align-items: center;
+            padding: 25px 10px 15px;
+        }
+
+        .logo img {
+            width: 30px;
+            border-radius: 50%;
+        }
+
+        .logo h2 {
+            font-size: 1.15rem;
+            font-weight: 600;
+            margin-left: 15px;
+            display: none;
+            margin-top: 8px;
+        }
+
+        .sidebar:hover .logo h2 {
+            display: block;
+        }
+
+        .sidebar .links {
+            list-style: none;
+            margin-top: 20px;
+            overflow-y: auto;
+            scrollbar-width: none;
+            height: calc(100% - 140px);
+        }
+
+        .sidebar .links::-webkit-scrollbar {
+            display: none;
+        }
+
+        .links li {
+            display: flex;
+            border-radius: 4px;
+            align-items: center;
+        }
+
+        .links li:hover {
+            cursor: pointer;
+            background: #fff;
+        }
+
+        .links h4 {
+            color: #222;
+            font-weight: 500;
+            display: none;
+            margin-bottom: 10px;
+        }
+
+        .sidebar:hover .links h4 {
+            display: block;
+        }
+
+        .links hr {
+            margin: 10px 8px;
+            border: 1px solid #4c4c4c;
+        }
+
+        .sidebar:hover .links hr {
+            border-color: transparent;
+        }
+
+        .links li span {
+            padding: 12px 10px;
+        }
+
+        .links li a {
+            padding: 10px;
+            color: #000;
+            display: none;
+            font-weight: 500;
+            white-space: nowrap;
+            text-decoration: none;
+        }
+
+        .sidebar:hover .links li a {
+            display: block;
+        }
+
+        .links .logout-link {
+            margin-top: 20px;
+        }
     </style>
 </head>
 <body>
     <nav class="navbar">
-    <a class="navbar-brand" href="#">Cholera</a>
+    <a href="admin-home.php"><img src="media/pharmacy.png" style="width:40px;height:40px;"></a>
     <button type="button" class="btn btn-light"><a href="logout-user.php">Logout</a></button>
     </nav>
     <h1>Welcome Admin - <?php echo $fetch_info['name'] ?></h1>
-    
+    <aside class="sidebar">
+      <div class="logo">
+      <img src="media/pharmacy.png" alt="logo">
+        <h2>CholeraCare</h2>
+      </div>
+      <ul class="links">
+        
+        <li>
+          <span class="material-symbols-outlined">dashboard</span>
+          <a href="#">Dashboard</a>
+        </li>
+        <li>
+          <span class="material-symbols-outlined">show_chart</span>
+          <a href="#">Revenue</a>
+        </li>
+        <li>
+          <span class="material-symbols-outlined">flag</span>
+          <a href="#">Reports</a>
+        </li>
+        <hr>
+        
+        <li>
+          <span class="material-symbols-outlined">person</span>
+          <a href="#">Designer</a>
+        </li>
+        <li>
+          <span class="material-symbols-outlined">group</span>
+          <a href="#">Developer </a>
+        </li>
+        <li>
+          <span class="material-symbols-outlined">ambient_screen</span>
+          <a href="#">Magic Build</a>
+        </li>
+        <li>
+          <span class="material-symbols-outlined">pacemaker</span>
+          <a href="#">Theme Maker</a>
+        </li>
+        <li>
+          <span class="material-symbols-outlined">monitoring</span>
+          <a href="#">Analytic</a>
+        </li>
+        <hr>
+        
+        <li>
+          <span class="material-symbols-outlined">bar_chart</span>
+          <a href="#">Overview</a>
+        </li>
+        <li>
+          <span class="material-symbols-outlined">mail</span>
+          <a href="#">Message</a>
+        </li>
+        <li>
+          <span class="material-symbols-outlined">settings</span>
+          <a href="#">Settings</a>
+        </li>
+        <li class="logout-link">
+          <span class="material-symbols-outlined">logout</span>
+          <a href="#">Logout</a>
+        </li>
+      </ul>
+    </aside>
 </body>
 </html>
