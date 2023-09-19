a0fad455db699c6495caffe0d143b39a28bdaf7f
HakeemAlavi <hakeem.alavi@strathmore.edu>
2023-09-19 13:34:06 +0300

    Added a dashboard card, deleted admin panel and added users panel instead

--- admin/view-users.php
+++ admin/view-users.php
@@ -1,38 +1,14 @@
-<?php require_once "controllerUserData.php"; ?>
-<?php 
-$email = $_SESSION['email'];
-$password = $_SESSION['password'];
-if($email != false && $password != false){
-    $sql = "SELECT * FROM usertable WHERE email = '$email'";
-    $run_Sql = mysqli_query($con, $sql);
-    if($run_Sql){
-        $fetch_info = mysqli_fetch_assoc($run_Sql);
-        $status = $fetch_info['status'];
-        $code = $fetch_info['code'];
-        if($status == "verified"){
-            if($code != 0){
-                header('Location: reset-code.php');
-            }
-        }else{
-            header('Location: user-otp.php');
-        }
-    }
-}else{
-    header('Location: login-user.php');
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title><?php echo $fetch_info['name'] ?> | Home</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
-    @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
-    nav{
+        @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+        nav{
         padding-left: 50px!important;
         padding-right: 50px!important;
         padding-top: 15px!important;
@@ -53,26 +29,61 @@ if($email != false && $password != false){
     button a:hover{
         text-decoration: none;
     }
-    h1{
-        position: absolute;
-        top: 20%;
-        left: 50%;
-        width: 100%;
-        text-align: center;
-        transform: translate(-50%, -62%);
-        font-size: 28px;
-        font-weight: 600;
-        font-family: 'Poppins';
-    }
-    * {
+    
+        h1 {
+            width: 100%;
+            text-align: center;
+            font-size: 28px;
+            font-weight: 600;
+            font-family: 'Poppins';
+            padding: 30px;
+            
+        }
+        * {
+            font-family: "Poppins";
+        }
+        .users {
+            width: fit-content;
+            transform: translate(40%, 0%);
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            border-spacing: 0;
+            box-shadow: 0 2px 15px rgba(64, 64, 64, .7);
+            border-radius: 12px 12px 0 0;
+            
+        }
+
+        td,
+        th {
+            padding:10px 16px;
+            text-align: center;
+        }
+
+        th {
+            background-color: #3deb6c;
+            color: #fafafa;
+            font-family: 'Poppins';
+            font-weight: bold;
+        }
+
+        tr {
+            width: 100%;
+            background-color: #fafafa;
+            
+        }
+
+        tr:nth-child(even) {
+            background-color: #eeeeee;
+        }
+
+        * {
             margin: 0;
             padding: 0;
             box-sizing: border-box;
             font-family: "Poppins", sans-serif;
         }
-
-        
-
         .sidebar {
             position: fixed;
             top: 0;
@@ -180,78 +191,63 @@ if($email != false && $password != false){
         .links .logout-link {
             margin-top: 20px;
         }
-
-        .card{
-            background-color: #3deb6c;
-            padding:20px;
-            margin: 10px;
-            border-radius: 10px;
-            box-shadow: 8px 5px 5px #D3D3D3;
-            transform: translate(40%, 60%);
-        }
-        .card h4 {
-            padding-top: 10px;
-            color: #222;
-            font-weight: 600;
-            font-family: 'Poppins';
-        } 
-        .card h5 {
-            color: #222;
-            font-weight: 600;
-            font-family: 'Poppins';
-            font-style: italic;
-        }     
-        
     </style>
 </head>
-<body>
-        <?php   
-            include_once "connection.php";
-        ?>
-    <nav class="navbar">
-    <a href="admin-home.php"><img src="media/pharmacy.png" style="width:40px;height:40px;"></a>
+<body> 
+<nav class="navbar">
+    <a href="../admin-home.php"><img src="../media/pharmacy.png" style="width:40px;height:40px;"></a>
     <button type="button" class="btn btn-light"><a href="logout-user.php">Logout</a></button>
-    </nav>
-    <h1>Welcome Admin - <?php echo $fetch_info['name'] ?></h1>
-
-    <div id="main-content" class="container allContent-section py-4">
-        <div class="row">
-            <div class="col-sm-3">
-                <div class="card">
-                    <i class="fa fa-users  mb-2" style="color: #ffffff";></i>
-                    <h4 style="color:white;">Total Users</h4>
-                    <h5 style="color:white;">
-                    <?php
-                        $sql="SELECT * from usertable WHERE authorization = 'user'";
-                        $result=$con-> query($sql);
-                        $count=0;
-                        if ($result-> num_rows > 0){
-                            while ($row=$result-> fetch_assoc()) {
-                    
-                                $count=$count+1;
-                            }
-                        }
-                        echo $count;
-                    ?></h5>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <aside class="sidebar">
+</nav>       
+<div class="users" >
+  <h1>All Users</h1>
+  <table class="table">
+    <thead>
+      <tr>
+        <th class="text-center">No.</th>
+        <th class="text-center">Name </th>
+        <th class="text-center">Email</th>
+        <th class="text-center">Verification Status</th>
+        <th class="text-center">Role</th>
+      </tr>
+    </thead>
+    <?php
+      include_once "../connection.php";
+      $sql="SELECT * from usertable WHERE authorization = 'user'";
+      $result=$con-> query($sql);
+      $count=1;
+      if ($result-> num_rows > 0){
+        while ($row=$result-> fetch_assoc()) {
+           
+    ?>
+    <tr>
+      <td><?=$count?></td>
+      <td><?=$row["name"]?></td>
+      <td><?=$row["email"]?></td>
+      <td><?=$row["status"]?></td>
+      <td><?=$row["authorization"]?></td>
+    </tr>
+    <?php
+            $count=$count+1;
+           
+        }
+    }
+    ?>
+  </table>
+</div>
+<aside class="sidebar">
       <div class="logo">
-      <img src="media/pharmacy.png" alt="logo">
+      <img src="../media/pharmacy.png" alt="logo">
         <h2>CholeraCare</h2>
       </div>
       <ul class="links">
       <li>
           <span class="material-symbols-outlined">dashboard</span>
-          <a href="admin-home.php">Dashboard</a>
+          <a href="../admin-home.php">Dashboard</a>
         </li>
         <li>
         <li>
           <span class="material-symbols-outlined">group</span>
-          <a href="admin/view-users.php">Users Panel </a>
+          <a href="view-users.php">Users Panel </a>
         </li>
         <li>
           <span class="material-symbols-outlined">show_chart</span>
@@ -300,12 +296,11 @@ if($email != false && $password != false){
         </li>
       </ul>
     </aside>
-    
 
-    <script type="text/javascript" src="./assets/js/ajaxWork.js"></script>    
-    <script type="text/javascript" src="./assets/js/script.js"></script>
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" ></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="./assets/js/ajaxWork.js"></script>    
+<script type="text/javascript" src="./assets/js/script.js"></script>
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
