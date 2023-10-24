9b144253988435f28e7b71843925d5b7bdbe423d
HakeemAlavi <hakeem.alavi@strathmore.edu>
2023-10-24 17:13:03 +0300

    Created a profile page for users

--- user/edit.php
+++ user/edit.php
@@ -1,33 +1,45 @@
-<?php require_once "controllerUserData.php"; ?>
-<?php 
-$email = $_SESSION['email'];
-$password = $_SESSION['password'];
-if($email != false && $password != false){
-    $sql = "SELECT * FROM usertable WHERE email = '$email'";
-    $run_Sql = mysqli_query($con, $sql);
-    if($run_Sql){
-        $fetch_info = mysqli_fetch_assoc($run_Sql);
-        $status = $fetch_info['status'];
-        $code = $fetch_info['code'];
-        if($status == "verified"){
-            if($code != 0){
-                header('Location: reset-code.php');
-            }
-        }else{
-            header('Location: user-otp.php');
-        }
-    }
-}else{
-    header('Location: login-user.php');
+<?php
+include "../connection.php";
+$id = $_GET["id"];
+
+if (isset($_POST["submit"])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  
+
+  $sql = "UPDATE `usertable` SET `name`='$name',`email`='$email' WHERE id = $id";
+
+  $result = mysqli_query($con, $sql);
+
+  if ($result) {
+    header("Location: profile.php?msg=Data updated successfully");
+  } else {
+    echo "Failed: " . mysqli_error($con);
+  }
 }
+
 ?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title><?php echo $fetch_info['name'] ?> | Home</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Bootstrap -->
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
     @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
     nav{
@@ -51,19 +63,18 @@ if($email != false && $password != false){
     button a:hover{
         text-decoration: none;
     }
-    h1{
+    /* h1{
         position: absolute;
-        top: 50%;
+        top: 20%;
         left: 50%;
         width: 100%;
         text-align: center;
-        transform: translate(-50%, -50%);
-        font-size: 50px;
+        transform: translate(-50%, -62%);
+        font-size: 28px;
         font-weight: 600;
-    }
-    /* Importing Google font - Poppins */
-        
-        * {
+        font-family: 'Poppins';
+    } */
+    * {
             margin: 0;
             padding: 0;
             box-sizing: border-box;
@@ -170,6 +181,7 @@ if($email != false && $password != false){
             font-weight: 500;
             white-space: nowrap;
             text-decoration: none;
+            
         }
 
         .sidebar:hover .links li a {
@@ -179,63 +191,105 @@ if($email != false && $password != false){
         .links .logout-link {
             margin-top: 20px;
         }
-    </style>
+
+        .container {
+            transform: translate(4.5%, 15%);
+            padding-top: 5px;
+        }
+
+        a{
+          padding: 5px;
+        }
+        
+    </style>  
+
+  <title>My Profile</title>
 </head>
+
 <body>
-    <nav class="navbar">
-    <a href="home.php"><img src="media/pharmacy.png" style="width:40px;height:40px;"></a>
-    
-    <button type="button" class="btn btn-light"><a href="logout-user.php">Logout</a></button>
+<nav class="navbar">
+    <a href="../home.php"><img src="../media/pharmacy.png" style="width:40px;height:40px;"></a>
+    <button type="button" class="btn btn-light"><a href="../logout-user.php">Logout</a></button>
     </nav>
-    <h1>Welcome <?php echo $fetch_info['name'] ?></h1>
-    <aside class="sidebar">
+
+  <div class="container">
+    <div class="text-center mb-4">
+      <h3>Edit My Information</h3>
+      <p class="text-muted">Click update after changing any information</p>
+    </div>
+
+    <?php
+    $sql = "SELECT * FROM `usertable` WHERE id = $id LIMIT 1";
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_assoc($result);
+    ?>
+
+    <div class="container d-flex justify-content-center">
+      <form action="" method="post" style="width:50vw; min-width:300px;">
+        <div class="row mb-3">
+          <div class="col">
+            <label class="form-label">Name:</label>
+            <input type="text" class="form-control" name="name" value="<?php echo $row['name'] ?>">
+          </div>
+
+          <div class="col">
+            <label class="form-label">Email:</label>
+            <input type="email" class="form-control" name="email" value="<?php echo $row['email'] ?>">
+          </div>
+        </div>
+
+        <!-- <div class="mb-3">
+          <label class="form-label">Authorization:</label>
+          <input type="text" class="form-control" name="authorization" value="<?php echo $row['authorization'] ?>">
+        </div> -->
+
+        <div>
+          <button type="submit" class="btn btn-success" name="submit">Update</button>
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          <a href="profile.php" class="btn btn-danger">Cancel</a>
+        </div>
+      </form>
+    </div>
+  </div>
+  <aside class="sidebar">
       <div class="logo">
-      <img src="media/pharmacy.png" alt="logo">
+      <img src="../media/pharmacy.png" alt="logo">
         <h2>CholeraCare</h2>
       </div>
       <ul class="links">
         <li>
-          <span class="material-symbols-outlined">home</span>
-          <a href="home.php">My Home</a>
-        </li>
-        <li>
-            <span class="material-symbols-outlined">pacemaker</span>
-            <a href="#" onclick="openCenteredWindow('https://hakeemalavi.shinyapps.io/Cholera_Model/', 'myWindow', 900, 630); return false;">Cholera Model</a>
-        </li>
-        
-        <script>
-            function openCenteredWindow(url, name, width, height) {
-                var left = (screen.width / 2) - (width / 2);
-                var top = (screen.height / 2) - (height / 2);
-                var options = 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left;
-                window.open(url, name, options);
-            }
-        </script>
-
-        
-
-        
-        <li>
           <span class="material-symbols-outlined">dashboard</span>
-          <a href="#">Dashboard</a>
+          <a href="../admin-home.php">Dashboard</a>
+        </li>
+        <li>
+        <li>
+          <span class="material-symbols-outlined">group</span>
+          <a href="user-panel.php">Users Panel </a>
         </li>
         <li>
           <span class="material-symbols-outlined">show_chart</span>
           <a href="#">Revenue</a>
         </li>
-        
-        <hr>
-        <li>
-          <span class="material-symbols-outlined">person</span>
-          <a href="user/profile.php">My Profile</a>
-        </li>
         <li>
           <span class="material-symbols-outlined">flag</span>
           <a href="#">Reports</a>
         </li>
+        <hr>
+        
         <li>
-          <span class="material-symbols-outlined">group</span>
-          <a href="#">Developer </a>
+          <span class="material-symbols-outlined">person</span>
+          <a href="#">Designer</a>
+        </li>
+        
+        <li>
+          <span class="material-symbols-outlined">ambient_screen</span>
+          <a href="#">Magic Build</a>
+        </li>
+        <li>
+          <span class="material-symbols-outlined">pacemaker</span>
+          <a href="#">Theme Maker</a>
         </li>
         <li>
           <span class="material-symbols-outlined">monitoring</span>
@@ -261,5 +315,12 @@ if($email != false && $password != false){
         </li>
       </ul>
     </aside>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>         
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </body>
+
 </html>
--- user/profile.php
+++ user/profile.php
@@ -1,4 +1,7 @@
-<?php require_once "controllerUserData.php"; ?>
+<?php
+include "../connection.php";
+?>
+<?php require_once "../controllerUserData.php"; ?>
 <?php 
 $email = $_SESSION['email'];
 $password = $_SESSION['password'];
@@ -21,13 +24,24 @@ if($email != false && $password != false){
     header('Location: login-user.php');
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title><?php echo $fetch_info['name'] ?> | Home</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Bootstrap -->
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
     @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
     nav{
@@ -51,19 +65,18 @@ if($email != false && $password != false){
     button a:hover{
         text-decoration: none;
     }
-    h1{
+    /* h1{
         position: absolute;
-        top: 50%;
+        top: 20%;
         left: 50%;
         width: 100%;
         text-align: center;
-        transform: translate(-50%, -50%);
-        font-size: 50px;
+        transform: translate(-50%, -62%);
+        font-size: 28px;
         font-weight: 600;
-    }
-    /* Importing Google font - Poppins */
-        
-        * {
+        font-family: 'Poppins';
+    } */
+    * {
             margin: 0;
             padding: 0;
             box-sizing: border-box;
@@ -170,6 +183,7 @@ if($email != false && $password != false){
             font-weight: 500;
             white-space: nowrap;
             text-decoration: none;
+            
         }
 
         .sidebar:hover .links li a {
@@ -179,24 +193,85 @@ if($email != false && $password != false){
         .links .logout-link {
             margin-top: 20px;
         }
-    </style>
+
+        .container {
+            transform: translate(4.5%, 15%);
+            padding-top: 80px;
+        }
+
+        a{
+          padding: 5px;
+        }
+        
+    </style>  
+  <title>My Profile</title>
 </head>
+
 <body>
-    <nav class="navbar">
-    <a href="home.php"><img src="media/pharmacy.png" style="width:40px;height:40px;"></a>
-    
-    <button type="button" class="btn btn-light"><a href="logout-user.php">Logout</a></button>
+<nav class="navbar">
+    <a href="../home.php"><img src="../media/pharmacy.png" style="width:40px;height:40px;"></a>
+    <button type="button" class="btn btn-light"><a href="../logout-user.php">Logout</a></button>
     </nav>
-    <h1>Welcome <?php echo $fetch_info['name'] ?></h1>
+
+    <div class="container">
+    <?php
+    if (isset($_GET["msg"])) {
+      $msg = $_GET["msg"];
+      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+      ' . $msg . '
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+    }
+    ?>
+    
+
+    <table class="table table-hover text-center">
+      <thead class="table-light">
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Status</th>
+          <th scope="col">Authorization</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php
+          // Add the filter to retrieve data only for the current session user
+          if (isset($_SESSION['email'])) {
+              $userEmail = $_SESSION['email'];
+              $sql = "SELECT * FROM `usertable` WHERE email = '$userEmail'";
+              $result = mysqli_query($con, $sql);
+              while ($row = mysqli_fetch_assoc($result)) {
+                  ?>
+                  <tr>
+                    <td><?php echo $row["id"] ?></td>
+                    <td><?php echo $row["name"] ?></td>
+                    <td><?php echo $row["email"] ?></td>
+                    <td><?php echo $row["status"] ?></td>
+                    <td><?php echo $row["authorization"] ?></td>
+                    <td>
+                    <a href="edit.php?id=<?php echo $row["id"] ?>" class="link-dark me-1"><i class="fa-solid fa-pen-to-square fs-5 me-3" style="color: #14A44D"></i></a>
+                    </td>
+                  </tr>
+          <?php
+        }
+      }
+    ?>
+      </tbody>
+    </table>
+  </div>   
+
     <aside class="sidebar">
       <div class="logo">
-      <img src="media/pharmacy.png" alt="logo">
+      <img src="../media/pharmacy.png" alt="logo">
         <h2>CholeraCare</h2>
       </div>
       <ul class="links">
         <li>
           <span class="material-symbols-outlined">home</span>
-          <a href="home.php">My Home</a>
+          <a href="../home.php">My Home</a>
         </li>
         <li>
             <span class="material-symbols-outlined">pacemaker</span>
@@ -227,7 +302,7 @@ if($email != false && $password != false){
         <hr>
         <li>
           <span class="material-symbols-outlined">person</span>
-          <a href="user/profile.php">My Profile</a>
+          <a href="profile.php">My Profile</a>
         </li>
         <li>
           <span class="material-symbols-outlined">flag</span>
@@ -261,5 +336,12 @@ if($email != false && $password != false){
         </li>
       </ul>
     </aside>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>      
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </body>
+
 </html>
